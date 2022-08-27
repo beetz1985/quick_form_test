575a5cdb8eb9f7e8856ed6ca31be3bea1cf5b0c2
beetz1985 <benjamintoulson@hotmail.co.uk>
2022-08-27 17:25:31 +0100

    Quick React Form Test Brush Up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    const [state, setState] = React.useState({
+                                    email: "",
+                                    password: "",
+                                    passwordConfirm: "",
+                                    okayToEmail: false
+                                })
+
+
+
+    function handleChange(e) { 
+
+        const {name, type, value, checked} = e.target;
+
+        setState((s)=>{
+            const obj = {...s}
+            obj[name] = (type === "checkbox") ? checked : value
+            return obj
+        }) 
+
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if(state.password !== state.passwordConfirm) {
+            console.log("Error, passwords do not match");
+            
+        } else if(state.email === "" || state.password === "" || state.passwordConfirm === "") {
+
+            console.log("Please fill out all required fields")
+
+        } else if(state.okayToEmail){
+
+            console.log(
+                `Succesfully Signed Up, Thankyou for choosing our newsletter`
+            )
+        } else {
+            console.log(
+                `Succesfully Signed Up`
+            )
+        }
+    }
+
+    //console.log(state);
+
+    return (
+        <div className="form-container">
+            <form className="form" onSubmit={handleSubmit}>
+                <input 
+                    type="email"
+                    name="email" 
+                    placeholder="Email address"
+                    className="form--input"
+                    onChange={handleChange}
+                />
+                <input 
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="form--input"
+                    onChange={handleChange}
+                />
+                <input 
+                    type="password" 
+                    name="passwordConfirm"
+                    placeholder="Confirm password"
+                    className="form--input"
+                    onChange={handleChange}
+                />
+                
+                <div className="form--marketing">
+                    <input
+                        id="okayToEmail"
+                        type="checkbox"
+                        name="okayToEmail"
+                        onChange={handleChange}
+                        
+                    />
+                    <label htmlFor="okayToEmail">I want to join the newsletter</label>
+                </div>
+                <button 
+                    className="form--submit"
+                >
+                    Sign up
+                </button>
+            </form>
+        </div>
+    )
 }
 
-export default App;
+export default App
